a6016576769fee3d7ba3f780a7c1c49e2d7bea0c
ucchyocean <ocean163@hotmail.com>
2013-10-15 20:47:53 +0900

    ColorTeamingClassSign v0.3.0 : Updated for ColorTeaming v2.2.5

--- src/main/java/com/github/ucchyocean/ctcs/ColorTeamingBridge.java
+++ src/main/java/com/github/ucchyocean/ctcs/ColorTeamingBridge.java
@@ -8,13 +8,10 @@ package com.github.ucchyocean.ctcs;
 import java.util.ArrayList;
 
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionEffect;
 
 import com.github.ucchyocean.ct.ColorTeaming;
-import com.github.ucchyocean.ct.Utility;
-import com.github.ucchyocean.ct.config.ClassData;
+import com.github.ucchyocean.ct.ColorTeamingAPI;
 import com.github.ucchyocean.ct.config.ColorTeamingConfig;
 
 /**
@@ -51,82 +48,11 @@ public class ColorTeamingBridge {
      */
     public boolean setClassToPlayer(Player player, String name) {
         
-        ColorTeamingConfig config = colorteaming.getCTConfig();
-        ClassData cdata = config.getClasses().get(name);
-
-        if ( cdata == null ) {
-            return false;
-        }
+        ColorTeamingAPI api = colorteaming.getAPI();
         
-        ArrayList<ItemStack> itemData = cdata.getItems();
-        ArrayList<ItemStack> armorData = cdata.getArmor();
-        ArrayList<PotionEffect> effectData = cdata.getEffect();
-        int experience = cdata.getExperience();
-        int level = cdata.getLevel();
+        ArrayList<Player> playersToSet = new ArrayList<Player>();
+        playersToSet.add(player);
         
-        // 全回復の実行
-        if ( config.isHealOnSetClass() ) {
-            Utility.heal(player);
-        }
-        
-        // インベントリの消去
-        player.getInventory().clear();
-        player.getInventory().setHelmet(null);
-        player.getInventory().setChestplate(null);
-        player.getInventory().setLeggings(null);
-        player.getInventory().setBoots(null);
-
-        // アイテムの配布
-        for ( ItemStack item : itemData ) {
-            if ( item != null ) {
-                player.getInventory().addItem(item);
-            }
-        }
-
-        // 防具の配布
-        if ( armorData != null ) {
-
-            if (armorData.size() >= 1 && armorData.get(0) != null ) {
-                player.getInventory().setHelmet(armorData.get(0));
-            }
-            if (armorData.size() >= 2 && armorData.get(1) != null ) {
-                player.getInventory().setChestplate(armorData.get(1));
-            }
-            if (armorData.size() >= 3 && armorData.get(2) != null ) {
-                player.getInventory().setLeggings(armorData.get(2));
-            }
-            if (armorData.size() >= 4 && armorData.get(3) != null ) {
-                player.getInventory().setBoots(armorData.get(3));
-            }
-        }
-        
-        // インベントリ更新
-        updateInventory(player);
-
-        // ポーション効果の設定
-        if ( effectData != null ) {
-            player.addPotionEffects(effectData);
-        }
-
-        // 経験値の設定
-        if ( experience != -1 ) {
-            player.setTotalExperience(experience);
-            Utility.updateExp(player);
-        } else if ( level != -1 ) {
-            player.setTotalExperience(0);
-            Utility.updateExp(player);
-            player.setLevel(level);
-        }
-        
-        return true;
-    }
-    
-    /**
-     * プレイヤーのインベントリをアップデートする
-     * @param player プレイヤー
-     */
-    @SuppressWarnings("deprecation")
-    private void updateInventory(Player player) {
-        player.updateInventory();
+        return api.setClassToPlayer(playersToSet, name);
     }
 }
